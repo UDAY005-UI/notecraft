23ec5ee47f58fd5554f89ee485a99cbc7b1b8f5f
Uday Senapati <udaysenapati6878@gmail.com>
2026-02-28 01:34:02 +0530

    refactor(payment): remove payment module and related utilities

--- api/src/types/payment.types.ts
+++
@@ -1,9 +0,0 @@
-export interface CreateOrderRequest {
-  amount: number;
-}
-
-export interface VerifyPaymentRequest {
-  razorpay_order_id: string;
-  razorpay_payment_id: string;
-  razorpay_signature: string;
-}

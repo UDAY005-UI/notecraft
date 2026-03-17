7587494919ab3a31a979c256294c5e32854a5270
Uday Senapati <udaysenapati6878@gmail.com>
2026-03-18 00:12:30 +0530

    resolve origin issue

--- api/src/index.ts
+++ api/src/index.ts
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: [
       "https://notecraft-two.vercel.app",
-      "http://notecrafts.in",
+      "https://notecrafts.in",
       "http://localhost:3000"
     ],
     credentials: true,

bc5b7049152abb4498583d6315c704cd7451c4bc
Uday Senapati <udaysenapati6878@gmail.com>
2026-03-18 00:46:23 +0530

    fix google adsense error

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
       <html lang="en">
         <head>
           <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1318066975388556"
-            crossOrigin="anonymous"></Script>
+            crossOrigin="anonymous" strategy="beforeInteractive"></Script>
         </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased `}
